d5c603dca20780648f9b280f03fd19c499113aeb
Yeshwanth <yeshwanth.nellikanti37@gmail.com>
2020-10-26 19:07:33 -0500

    Updated 1st part of Normalizarion

--- NORMALIZATION/Normalization.sql
+++ NORMALIZATION/Normalization.sql
@@ -63,5 +63,65 @@ Outline of this assignment
   6. JOIN new tables.
   7. Produce Relationship Diagram. Should look like schema in SQL Exercise System.
   8. Produce a set of Tokenized diagrams that illustrate the normalization process.
+*/
+/*********************************************************************
+1. Create a RELATION
+**********************************************************************/
+
+/*
+1a. Write the queries to demonstrate if your trip relation has duplicate rows.
+*/
+
+--Write your queries between the comments...
+--SELECT * FROM myTable WHERE predicate;
+
+/*
+Write the query to return the total number of rows imported.
+RowCnt
+171
+*/
+SELECT * FROM Trips 
+
+SELECT COUNT(*)AS NumberofRows
+FROM Trips;
+/*
+Write the query to return the total number of DISTINCT rows (should be 170)
+
+DistinctRowCnt
+170
+*/
+SELECT COUNT(*) DistinctRowCount
+FROM (SELECT DISTINCT * FROM Trips) AS DistinctRowCount;
+/*
+1b. Explain why your Trip relation is not a relation. Include the definition of a relation.
+*/
+-- Relation : Relation refers to a set of data in the Table.
+-- Trip relation is not a Relation because it has duplicate values. So it is not a relation.
+
+/*
+1c. Write a query to identify the duplicate row.
+
+TripStart               TripEnd                 TripHour TripEngineStart (and so on...)
+2010-08-08 12:00:00.000  2010-08-08 17:00:00.000  5        20.8
 
 */
+SELECT COUNT(*) As RowsCnt, TripStart, TripEnd, TripHour, TripEngineStart, TripEngineEnd, WaterTemp,MfgYear, Make,Model, ShipVIN,CaptainName, Pax1, Pax2, Pax3, Pax4, Pax5, Pax6, Pax7, Launch
+FROM Trips
+GROUP BY TripStart, TripEnd, TripHour, TripEngineStart, TripEngineEnd, WaterTemp,MfgYear, Make,Model, ShipVIN,CaptainName, Pax1, Pax2, Pax3, Pax4, Pax5, Pax6, Pax7, Launch
+HAVING COUNT(*) > 1;
+
+--1d.
+--Create new table without duplicate rows using INTO clause
+--Order the new table by TripStart ASC.
+SELECT TripStart, TripEnd, TripHour, TripEngineStart, TripEngineEnd, WaterTemp,MfgYear, Make,Model, ShipVIN,CaptainName, Pax1, Pax2, Pax3, Pax4, Pax5, Pax6, Pax7, Launch
+INTO Trips1
+FROM Trips
+GROUP BY TripStart, TripEnd, TripHour, TripEngineStart, TripEngineEnd, WaterTemp,MfgYear, Make,Model, ShipVIN,CaptainName, Pax1, Pax2, Pax3, Pax4, Pax5, Pax6, Pax7, Launch
+ORDER BY TripStart ASC;
+--Run a query to confirm the duplicate transaction has been removed (should only have 170 rows)
+SELECT * FROM Trips1;
+
+--1e. Write a comment that explains the query written for 1d. This should not simply restate the SQL syntax.
+
+-- In the above query we are creating a new table so that we can eliminate duplicate rows by removing COUNT function and HAVING function. In this way the duplicate gets deleted. 
+-- By adding into function GROUP BY the duplicate gets one.
